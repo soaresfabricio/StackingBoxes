3be1d3e408b8acbb49fe9edab1b3f5442c2a6395
Fabricio Soares <fabriciosoares@live.com>
2016-09-28 12:42:50 -0300

    Initial Commit

---
+++ BoxStacking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BoxStacking
+//
+//  Created by Fabrício Soares on 28/09/16.
+//  Copyright © 2016 Fabrício Soares. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
